c29376c15e7e81feb14ed32ed2789089a1511280
Vladislav <vladisgnatenko@gmail.com>
2023-07-30 15:00:21 +0300

    second
    second commit

--- MyPlayground 03.playground/Contents.swift
+++ MyPlayground 03.playground/Contents.swift
@@ -17,7 +17,7 @@ IAmOlder(15, 35)
 
 func yearOfBirth(yourAge: Int, currentYear: Int) {
     let yearOfBirth = currentYear - yourAge
-    print("Ти НАродився у \(yearOfBirth) році")
+    print("Ти Народився у \(yearOfBirth) році")
 }
 
 
@@ -28,9 +28,27 @@ yearOfBirth(yourAge: 26, currentYear: 2023)
 
 //onlyEvenNumbers - приймає range чисел та виводить у консоль тільки парні числа
 
+func onlyEvenNumbers(range: Range<Int>) {
+    for number in range {
+        if number % 2 == 0 {
+            print(number)
+        }
+    }
+}
+
+//func onlyEvenNumbers(range:Range<Int>) {
+//    var number = range.lowerBound
+//    while number < range.upperBound {
+//        if number % 2 == 0 {
+//            print(number)
+//        }
+//        number += 1
+//    }
+//}
 
 
 
+onlyEvenNumbers(range: 1..<10)
 
 
 
@@ -38,3 +56,14 @@ yearOfBirth(yourAge: 26, currentYear: 2023)
 
 
 //getSum - приймає 4 числа, з яких перші 2 додає, після чого множить на третє і знаходить залишок від поділу з четвертого числа - виводить у консоль та повертає
+
+func getSum(num1:Int,num2:Int,num3:Int,num4:Int) -> Int {
+    let sum = num1 + num2
+    let prod = sum * num3
+    let remain = prod % num4
+    
+    return remain
+}
+
+let result = getSum(num1: 5, num2: 8, num3: 4, num4: 9)
+print("Result\(result)")
